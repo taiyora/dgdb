72e0a6962a2306ac9d3f267d9ffaf753acb4e573
taiyora <taiyora@protonmail.com>
2018-02-06 00:23:41 +1300

    Redesign game view page; implement screenshots (URL only)

--- routes/index.js
+++ routes/index.js
@@ -39,6 +39,7 @@ router.get('/', function(req, res, next) {
 		title: websiteName + ' // home' });
 });
 
+// ----------------------------------------------------------------- game/list
 router.get('/game/list', function(req, res, next) {
 	const query = `SELECT * FROM games ORDER BY
 		title_english ASC,
@@ -61,8 +62,13 @@ router.get('/game/list', function(req, res, next) {
 	});
 });
 
+// ----------------------------------------------------------------- game/view
 router.get('/game/view/:id', function(req, res, next) {
-	const query = `SELECT * FROM games WHERE id = $1;`;
+	// Get all the information on the game, as well as its related screenshots
+	const query = `SELECT *,
+		array(SELECT url FROM screenshots WHERE game_id = $1) AS ss_urls
+		FROM games WHERE id = $1;`;
+
 	const vars = [ req.params.id ];
 
 	pgPool.query(query, vars, function(err, res2) {
@@ -89,11 +95,78 @@ router.get('/game/view/:id', function(req, res, next) {
 	});
 });
 
+// ----------------------------------------------------------------- game/new
 router.get('/game/new', function(req, res, next) {
 	res.render('game/new', {
 		title: websiteName + ' // new entry',
 		form: {} });
 });
+
+/**
+ * Saves a game entry to the database.
+ *
+ * @param {dict} form - Information about the game (from a form).
+ * @param {function(bool)} callback - The callback function.
+ */
+function saveGameEntry(form, callback) {
+	const query = `INSERT INTO games (
+		title_jp,
+		title_romaji,
+		title_english,
+		title_english_official,
+		title_other,
+		entry_created ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;`;
+
+	const vars = [
+		form.title_jp,
+		form.title_romaji,
+		form.title_english,
+		form.title_english_official ? 'TRUE' : 'FALSE',
+		form.title_other,
+		new Date(new Date().getTime()).toISOString() ];
+
+	pgPool.query(query, vars, function(err, res) {
+		if (err) {
+			console.error(err);
+			callback(false, 0);
+		}
+		else {
+			callback(true, res.rows[0].id);
+		}
+	});
+}
+
+/**
+ * Saves a list of screenshots to the database.
+ *
+ * @param {array} screenshots - An array of screenshot URLs.
+ * @param {int} gameId - The ID of the game the screenshot is related to.
+ */
+function saveScreenshots(screenshots, gameId) {
+	// Form the query string
+	let query = 'INSERT INTO screenshots (url, game_id) VALUES';
+	let vars = [ gameId ];
+	let n = 1;
+
+	screenshots.forEach(function(ss) {
+		n++;
+		if (n > 2) {
+			query += ',';
+		}
+
+		query += ' ($' + n + ', $1)';
+		vars.push(ss);
+	});
+
+	query += ';';
+
+	// Query the database
+	pgPool.query(query, vars, function(err, res) {
+		if (err) {
+			console.error(err);
+		}
+	});
+}
 
 router.post('/game/new', function(req, res, next) {
 	const form = req.body;
@@ -113,35 +186,37 @@ router.post('/game/new', function(req, res, next) {
 	}
 
 	else {
-		const query = `INSERT INTO games (
-			title_jp,
-			title_romaji,
-			title_english,
-			title_english_official,
-			title_other,
-			entry_created ) VALUES ($1, $2, $3, $4, $5, $6);`;
+		// Parse the list of screenshots
+		let screenshotsValidated = [];
 
-		const vars = [
-			form.title_jp,
-			form.title_romaji,
-			form.title_english,
-			form.title_english_official ? 'TRUE' : 'FALSE',
-			form.title_other,
-			new Date(new Date().getTime()).toISOString() ];
+		if (form.screenshots) {
+			// textareas use \r\n for newlines it seems
+			const screenshots = form.screenshots.split('\r\n');
 
-		pgPool.query(query, vars, function(err, res2) {
-			if (err) {
-				console.error(err);
+			// Validate that each given string is an image URL
+			screenshots.forEach(function(ss) {
+				if ( /^https?:\/\/.+\.(gif|png|jpg|jpeg)$/i.test(ss) ) {
+					screenshotsValidated.push(ss);
+				}
+				else {
+					error = 'At least one of the screenshot URLs is invalid';
+					return;
+				}
+			});
+		}
 
-				res.render('game/new', {
-					title: websiteName + ' // new entry',
-					error: 'Something went wrong; please try again',
-					form: form });
-			}
-			else {
-				res.redirect('/');
-			}
-		});
+		// If an error occurred while validating the screenshot URLs, we skip this
+		if (!error) {
+			saveGameEntry(form, function(success, gameId) {
+				if (success) {
+					saveScreenshots(screenshotsValidated, gameId);
+					res.redirect('/game/view/' + gameId);
+				}
+				else {
+					error = 'Something went wrong; please try again';
+				}
+			});
+		}
 	}
 
 	if (error) {
